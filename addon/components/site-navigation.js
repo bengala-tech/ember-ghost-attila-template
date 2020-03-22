9680cd201a3be53bdec75903409fd010ab0556b3
betocantu93 <betocantu93@gmail.com>
2020-03-22 17:23:17 -0600

    rm stuff

--- addon/components/site-navigation.js
+++ addon/components/site-navigation.js
@@ -1,20 +1,25 @@
-import Component from '@ember/component';
-import layout from '../templates/components/site-navigation';
-import { inject as service } from '@ember/service';
-import { computed } from '@ember/object';
+import Component from "@ember/component";
+import layout from "../templates/components/site-navigation";
+import { computed } from "@ember/object";
+import { getOwner } from "@ember/application";
 
 export default Component.extend({
-  blog: service(),
   layout,
+  
+  blog: computed(function() {
+    return getOwner(this).lookup("service:blog") || {};
+  }),
 
-  rssFeed: computed('blog.host', function() {
-    if(!this.blog.host) {
+  rssFeed: computed("blog.host", function() {
+    if (!this.blog.host) {
       return;
     }
-    return `https://feedly.com/i/subscription/feed${encodeURIComponent('/' + this.blog.host + '/rss.xml')}`;
+    return `https://feedly.com/i/subscription/feed${encodeURIComponent(
+      "/" + this.blog.host + "/rss.xml"
+    )}`;
   }),
 
   click() {
-    document.firstElementChild.classList.remove('menu-active');
+    document.firstElementChild.classList.remove("menu-active");
   }
 });
